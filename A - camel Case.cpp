50f1ac42782c19c5d8cca93f323a5fb931883153
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-17 22:32:53 +0600

    A - camel Case
    https://atcoder.jp/contests/abc291/submissions/me

---
+++ A - camel Case.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        string s;
+        cin>>s;
+
+        for(int i=0;i<s.size();i++){
+                int n=s[i];
+                if(n<97){
+                    cout<<i+1;
+                    break;
+                }
+
+
+
+        }
+
+}

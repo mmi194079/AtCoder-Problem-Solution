bae72c12585488f38012a3c9e5ee9d05bbbbe941
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-18 19:28:10 +0600

    A - Generalized ABC
    https://atcoder.jp/contests/abc282/submissions/me

---
+++ A - Generalized ABC.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+
+//            char c=i;
+            cout<<(char)(i+65);
+
+
+        }
+}

6d4e3cba57d637b7c529c697edf2eed34bdae928
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-26 19:35:23 +0600

    A - Rightmost
    https://atcoder.jp/contests/abc276/submissions/me

---
+++ A - Rightmost.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        string s;
+        cin>>s;
+
+        for(int i=(s.size())-1;i>=0;i--){
+            if(s[i]=='a'){
+            cout<<i;
+            break;
+
+            }
+
+        }
+}

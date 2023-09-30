8088710e0ba53d9e715ad5aa2b6fb9ef73943a16
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-30 10:19:20 +0600

    A - Saturday
    https://atcoder.jp/contests/abc267/submissions/me

---
+++ A - Saturday.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        //string t[] ={"Monday", "Tuesday", "Wednesday", "Thursday","Friday"};
+        string s;
+        cin>>s;
+
+
+            if(s=="Monday"){
+                cout<<5;
+
+            }
+            else if(s=="Tuesday"){
+                cout<<4;
+
+            }
+            else if(s=="Wednesday"){
+                cout<<3;
+
+            }
+            else if(s=="Thursday"){
+                cout<<2;
+
+            }
+            else if(s=="Friday"){
+                cout<<1;
+
+            }
+
+}

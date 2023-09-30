cddc6549a8610a944411b3a2df7819b8274d6489
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-30 09:35:58 +0600

    A - Five Integers
    https://atcoder.jp/contests/abc268/submissions/me

---
+++ A - Five Integers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int a;
+        set<int>s;
+        vector<int>v;
+        for(int i=0;i<5;i++){
+            cin>>a;
+            //v.push_back(a);
+            s.insert(a);
+        }
+
+       // int x=v.size();
+        int y = s.size();
+        cout<<y;
+
+
+
+}

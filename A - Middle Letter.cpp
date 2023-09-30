16f16c55a95238fe68c597bf4b68a6423b3fcf95
MD.Minhajul Islam <136610852+mmi194079@users.noreply.github.com>
2023-09-30 10:28:47 +0600

    A - Middle Letter
    https://atcoder.jp/contests/abc266/submissions/me

---
+++ A - Middle Letter.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        string s;
+        cin>>s;
+
+        float x=s.size();
+        cout<<s[ceil(x/2)-1];
+
+
+//        for(int i=0;i<s.size();i++){
+//            float a=
+//        }
+}
